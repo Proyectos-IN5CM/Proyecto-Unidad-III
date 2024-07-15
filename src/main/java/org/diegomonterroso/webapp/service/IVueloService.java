ed295c05302a361aa23168d3831941069d3404d6
AlexxoK <dmonterroso-2023234@kinal.edu.gt>
2024-07-14 18:38:45 -0600

    Avance de mi parte

---
+++ src/main/java/org/diegomonterroso/webapp/service/IVueloService.java
@@ -0,0 +1,17 @@
+package org.diegomonterroso.webapp.service;
+
+import java.util.List;
+import org.diegomonterroso.webapp.model.Vuelo;
+
+public interface IVueloService {
+    
+    public List<Vuelo> listarVuelos();
+    
+    public void agregarVuelo();
+    
+    public void eliminarVuelo(int vueloId);
+    
+    public void buscarVueloPorId(int vueloId);
+    
+    public void editarVuelo(Vuelo vuelo);
+}
