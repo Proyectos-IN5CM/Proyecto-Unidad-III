e13950f6a9e4f934e9e0d74924c85a3e53427d66
CarlosOrozcoK <corozco-2019392@kinal.edu.gt>
2024-07-15 10:39:34 -0600

    Avance propio orozco

---
+++ src/main/java/org/diegomonterroso/webapp/database/ProyectoUnidadIII.sql
@@ -0,0 +1,16 @@
+create database if not exists ProyectoUnidadIII;
+
+use ProyectoUnidadIII;
+
+create table Vuelos(
+	vueloId int not null auto_increment,
+    numeroVuelo int not null,
+    origen varchar(100),
+    destino varchar(100),
+    aerolinea varchar(100),
+    primary key PK_vueloId(vueloId)
+);
+
+select * from Vuelos;
+
+Set global time_zone = '-6:00';
