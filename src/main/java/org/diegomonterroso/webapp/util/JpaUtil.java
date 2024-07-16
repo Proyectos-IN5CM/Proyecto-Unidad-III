3ff8beb508f1565316a3c1ac94ec8af25e2acf7f
GerardoContreras06 <gerardocontrerasestrada@gmail.com>
2024-07-15 21:10:41 -0600

    Avance Gerardo Contreras

---
+++ src/main/java/org/diegomonterroso/webapp/util/JpaUtil.java
@@ -0,0 +1,26 @@
+package org.diegomonterroso.webapp.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+    private static final EntityManagerFactory emf = buildEntityManagerFactory();
+    
+    public static EntityManagerFactory buildEntityManagerFactory(){
+        try{
+            return Persistence.createEntityManagerFactory("ProyectoUnidadIII");
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+    
+    public static EntityManager getEntityManager(){
+        return emf.createEntityManager();
+    }
+    
+    public static void close(){
+        emf.close();
+    }
+}
